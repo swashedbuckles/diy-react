5832d5f8527b2d1bf22a8533be45f8c5394a6fa1
Tom Joseph <swashedbuckles@gmail.com>
2019-11-16 20:54:54 -0500

    a render function?

--- index.js
+++ index.js
@@ -1,5 +1,38 @@
-function render() {
-  
+/**
+ * @typedef {Object} element
+ * @property {string} type
+ * @property {Object.<string, string>} props
+ * @property {string|element|Array<string|element>} props.children
+ */
+
+/**
+ * @param {element} element
+ * @param {HTMLElement} container
+ */
+function render(element = {}, container) {
+  element.props = element.props || {};
+
+  const node = document.createElement(element.type);
+  const children = [].concat(element.props.children);
+  delete element.props.children;
+
+  const propNames = Object.keys(element.props);
+  propNames.forEach(name => node.setAttribute(name, element.props[name]));
+
+  children.forEach(child => {
+    if(typeof child === 'string') {
+      const text = document.createTextNode(child);
+      node.appendChild(text);
+    }
+
+    if(typeof child === 'object') {
+      render(child, node);
+    }
+  });
+
+  container.appendChild(node);
 }
 
-module.exports = render;
+module.exports = {
+  render
+};
--- index.test.js
+++ index.test.js
@@ -1,5 +1,8 @@
 const diyeact = require('./index');
 
 test('it should render an element', () => {
-
+  const element = `<h1 title="foo">Hello</h1>`;
+  const container = document.getElementById('root');
+  diyeact(element, container);
+  
 });
